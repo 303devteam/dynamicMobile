e48568d430d7c2b4c41053b8f020716bd719f00d
naimPJ <private.naim@gmail.com>
2023-10-29 02:20:01 +0100

    Tables header in progress

--- Tables.js
+++ Tables.js
@@ -1,3 +1,4 @@
+
 import { useCustomFonts } from "./assets/fonts/expo-fonts";
 import { StyleSheet, Text, View, Image, ScrollView } from 'react-native';
 
@@ -6,16 +7,30 @@ export default function Tables({navigation}) {
 
     return(
         <>
-            <Image
-                source={require('./assets/images/dynamiclogo.png')}
-            />
+            
+            <View style={styles.header}>
+                <View style={styles.leftContent}>
+                    <Image
+                        source={require('./assets/images/dynamiclogo.png')}
+                        style={{ width: 100, height: 100 }}
+                    />
+                </View>
+
+                <View style={styles.rightContent}>
+                    <Text style={styles.headerText}>Tables</Text>
+                    <View style={styles.poolTable}>
+                        <Image
+                            source={require('./assets/images/poolTable.png')}
+                            style={{ width: 60, height: 57 }}
+                        />
+                    </View>
+                </View>
+
+            </View>
+
 
             <View style={styles.container}>
-                <View style={styles.tablePic}>
-                <Image
-                    source={require('./assets/images/poolTable.png')}
-                />
-                </View>
+                
             </View>
 
         </>
@@ -25,6 +40,30 @@ export default function Tables({navigation}) {
 
 
 const styles = StyleSheet.create({
+    
+    headerText:{
+        flex: 1,
+        color: '#FFFFFF',
+        fontSize: 30,
+
+        
+    },
+    header:{
+        backgroundColor: '#201E21',
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        alignItems: 'center',
+        padding: 16
+        
+    },
+
+   
+    leftContent: {
+        flexDirection: 'row', 
+        alignItems: 'center',
+      },
+
+
     container: {
         flex: 1,
         backgroundColor: '#201E21',
@@ -33,9 +72,8 @@ const styles = StyleSheet.create({
         paddingTop: 100
       },
 
-    tablePic: {
-        width: 60,
-        
+    poolTable: {
+       
     }
 
 })
