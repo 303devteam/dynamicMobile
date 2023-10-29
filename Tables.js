5a136d163ff718a82efc090f972bb030e4286955
Tarik Maljanović <tarikmaljanovic123@gmail.com>
2023-10-29 02:01:23 +0200

    nesto radio

--- Tables.js
+++ Tables.js
@@ -1,18 +1,41 @@
 import { useCustomFonts } from "./assets/fonts/expo-fonts";
-import { StyleSheet, Text, View, Image } from 'react-native';
+import { StyleSheet, Text, View, Image, ScrollView } from 'react-native';
 
 export default function Tables({navigation}) {
     useCustomFonts()
 
     return(
         <>
-        <Image
-            source={require('./assets/images/dynamiclogo.png')}
-            style={styles.logo}
-        />
-        
+            <Image
+                source={require('./assets/images/dynamiclogo.png')}
+            />
+
+            <View style={styles.container}>
+                <View style={styles.tablePic}>
+                <Image
+                    source={require('./assets/images/poolTable.png')}
+                />
+                </View>
+            </View>
 
         </>
     );
        
 }
+
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: '#201E21',
+        alignItems: 'center',
+        justifyContent: 'center',
+        paddingTop: 100
+      },
+
+    tablePic: {
+        width: 60,
+        
+    }
+
+})
