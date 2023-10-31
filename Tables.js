c818e3c568e86763f696fe029b9013cbac2b2a98
naimPJ <private.naim@gmail.com>
2023-10-31 11:13:38 +0100

    Table's screen finalized

--- Tables.js
+++ Tables.js
@@ -24,18 +24,16 @@ export default function Tables({navigation}) {
                     <View style={styles.stack}>
                         {Array(10).fill().map((_, index) => (
                             <View key={index} style={styles.square}>
-                                <Text style={styles.squareText}>Table: {index + 1}</Text>
-                                <View style={styles.rightSquareContent}>
-                                    <View style={styles.circle}></View>
-                                    
+                                <View style={styles.textContainer}>
+                                    <Text style={styles.squareText}>Table: {index + 1}</Text>
                                 </View>
-
+                                <View style={styles.circle}></View>
+                                <Text style={styles.availabilityText}>Free</Text>
                             </View>
                         ))}
                     </View>
                 </View>
             </ScrollView>
-            
         </>
       
 
@@ -43,40 +41,34 @@ export default function Tables({navigation}) {
        
 }
 
-
 const styles = StyleSheet.create({
-    
+
     headerText:{
         flex: 1,
         color: '#FFFFFF',
         fontSize: 30,
         paddingRight: 10,
         paddingTop: 5,
-        paddingLeft: 8
-
-        
+        paddingLeft: 8        
     },
-    header:{
+    header:{    
         backgroundColor: '#201E21',
         flexDirection: 'row',
         justifyContent: 'space-between',
         alignItems: 'center',
         padding: 16,
-        marginTop: 50
-        
+        marginTop: 32     
+    },
 
-        
+    textContainer:{
+        flex:1   
     },
     
     headerContent:{
         flexDirection:'row',
         display: 'flex',
         justifyContent: 'space-between',
-        
     },
-
-   
-
 
     container: {
         flex: 1,
@@ -88,6 +80,7 @@ const styles = StyleSheet.create({
 
     square: {
         display: 'flex',
+        flexDirection: 'row',
         width: 349,
         height: 55,
         backgroundColor: 'white',
@@ -96,7 +89,6 @@ const styles = StyleSheet.create({
     },
 
     squareText:{
-        display: 'flex',
         textAlign: "left",
         marginLeft: 12,
         marginTop: 13,
@@ -104,13 +96,21 @@ const styles = StyleSheet.create({
     },
 
     circle:{
-        display: 'flex',
         height: 8,
         width: 8,
-        borderRadius: 80,
+        borderRadius: 4,
         backgroundColor: 'green',
-        marginLeft: 241,
-        
-    }
+        marginLeft: 150,
+        marginTop: 26,
+        textAlign: "center",
+        justifyContent: "center",   
+    },
 
+    availabilityText:{
+        fontSize: 14,
+        paddingTop: 20,
+        paddingLeft: 10,
+        paddingRight: 20,
+        color: 'green'
+    }
 })
